408cb81ae7ddc046e90a2fb3faaa465f07562723
Sasha Petrov <brat77@tut.by>
2021-08-17 19:40:41 +0300

    decide task

--- script.js
+++ script.js
@@ -1,46 +1,38 @@
 const postTitle = document.querySelector('.post__title');
 const postBody = document.querySelector('.post__body');
+const main = document.querySelector('main')
+let commentBody = document.createElement('div');     
+main.append(commentBody);
 
-const url = 'https://jsonplaceholder.typicode.com/posts';
+fetch('https://jsonplaceholder.typicode.com/posts')
+ .then( response => {return  response.json()})
+ .then(data =>{
+    let id = 0;
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    		.then( dataCom => dataCom.json())
+    		.then(res =>{
+          console.log(res);
+    let btnChangePost = document.querySelector(".post__btn_change");
+        btnChangePost.addEventListener("click", () => {
+          
+          postTitle.innerHTML = id + '  ' + data[id].title;
+          postBody.innerHTML = data[id].body;
 
-fetch(url)
-.then( function(response) {  
-  // if (response.status !== 200) {  
-  //   console.log('Looks like there was a problem. Status Code: ' +  
-  //     response.status);  
-  //   return;  
-  // }
-	response.json()
- 	.then(function(data) {  
-        id = 0;
-        postTitle.innerHTML = id + '  ' + data[id].title;
-        postBody.innerHTML = data[id].body;
+          // ********* comments ************
+          
+          commentBody.remove();
+          commentBody = document.createElement('div');  
+          main.append(commentBody);
 
-	        let btnChangePost = document.querySelector(".post__btn_change");
-	      		btnChangePost.addEventListener("click", () => {
-			        id ++;
-			  		if (!data[id]) {
-				      id = 0;
-			    	}
-			    postTitle.innerHTML = id + '  ' + data[id].title;
-			    postBody.innerHTML = data[id].body;
- 				});
+          for ( let i = id*5; i < id*5+5; i++ ) {
+            let comment = document.createElement('p');
+            commentBody.append(comment);
+            comment.className = ('comment');
+            comment.innerHTML = res[i].body;
+          }
+          id ++;
+        })
+      })
+       
 
-	      	
-
-	  	})
-	}  
-  ) 
-// ********* comments ************
-// fetch(url + '/1/comments')
-// 		.then( function(data) {
-// 			data.json()
-// 		})
-// 		.then((json) =>{console.log(json);})
-  
-// let btnPostCommit = document.querySelector(".post__btn_commit");
-// 	btnPostCommit.addEventListener('click', () => {
-// 		console.log('777777');
-		
-// 	});
-
+})
